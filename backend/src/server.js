94dd52407736afb3e9576762e549b8d6a47f17d4
riyanshugupta74 <guptariyanshu86@gmail.com>
2026-09-13 20:31:43 +0530

    express 5 issue fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -57,7 +57,7 @@ const frontendPath = path.join(__dirname, '../../dist');
 
 app.use(express.static(frontendPath));
 
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
